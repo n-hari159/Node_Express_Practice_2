20f3eb20311db99761d961ee2b19cff464a42723
n-hari159 <n.hari159@gmail.com>
2019-04-16 22:47:43 +0530

    updated on working with os module

--- First_App/app.js
+++ First_App/app.js
@@ -4,9 +4,19 @@
 
 // log('message');
 
+// Working with path module
+// const path = require('path');
 
-const path = require('path');
+// var pathObj = path.parse(__filename);
 
-var pathObj = path.parse(__filename);
+// console.log(pathObj);
 
-console.log(pathObj);
+
+// working with OS module
+const os  = require('os');
+
+var totalmMemory = os.totalmem();
+var freeMemory = os.freemem();
+
+console.log(`total memory : ${totalmMemory}`);
+console.log(`free memory : ${freeMemory}`);
