09dea2d41172393c7bace271cc0caf86b396bb1c
n-hari159 <n.hari159@gmail.com>
2019-04-18 02:57:23 +0530

    Creating Schema

--- mongo_test/index.js
+++ mongo_test/index.js
@@ -1,5 +1,16 @@
 const mongoose = require('mongoose');
 
+// Connecting to MongoDB
 mongoose.connect('mongodb://localhost/playground')
     .then(() => console.log('Connected to MongoDB....'))
     .catch(err => console.error('Could not connect to MongoDB', err ));
+
+
+// Schema for document in MongoDB
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
