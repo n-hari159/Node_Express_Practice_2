a2004ca55193690b0b5ad87d12d8781402b17cea
n-hari159 <n.hari159@gmail.com>
2019-04-17 17:29:01 +0530

    working with promises

---
+++ async_test/promise.js
@@ -0,0 +1,15 @@
+
+// Promise will be in pending state initially
+const p = new Promise((resolve, reject) => {
+    // Kick off some async work
+    setTimeout( () => {
+        // resolve(1); // pending => resolved/fulfilled
+        reject(new Error('message')); // pending => rejected
+
+    }, 2000);
+    
+});
+
+p
+    .then(result => console.log('Result', result))
+    .catch(err => console.log('Error', err.message));
