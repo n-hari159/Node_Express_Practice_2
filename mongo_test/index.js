d00233ad098ae8d946fe5603d1252a368ae72318
n-hari159 <n.hari159@gmail.com>
2019-04-18 03:24:43 +0530

    Querying Documents

--- mongo_test/index.js
+++ mongo_test/index.js
@@ -20,9 +20,9 @@ const Course = mongoose.model('Course', courseSchema);
 
 async function createCourse() {
     const course = new Course({
-        name: 'Node.js Course',
+        name: 'Angular Course',
         author: 'Hari Nakka',
-        tags: ['node', 'backend'],
+        tags: ['angular', 'frontend'],
         isPublished: true
     });
     
@@ -31,4 +31,16 @@ async function createCourse() {
     console.log(result);
 }
 
-createCourse();
+// Querying Documents
+async function getCourses() {
+    const courses = await Course
+        .find({ author: 'Hari Nakka', isPublished: true })
+        .limit(10)
+        .sort({ name: -1 })
+        .select({ name: 1, tags: 1 });
+    console.log(courses);
+}
+
+getCourses();
+
+// createCourse();
