b01d325cf178706360f8a043cc375f57b0cba5d4
n-hari159 <n.hari159@gmail.com>
2019-04-19 21:02:14 +0530

    updated

--- vidly/routes/genres.js
+++ vidly/routes/genres.js
@@ -1,13 +1,20 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-const genres = [
-  { id: 1, name: 'Action' },  
-  { id: 2, name: 'Horror' },  
-  { id: 3, name: 'Romance' },  
-];
+
+const Genre = new mongoose.model('Genre', new mongoose.schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  }
+}));
+
   
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const genres = await Genre.find().sort('name');
   res.send(genres);
 });
   
@@ -15,18 +22,16 @@ router.post('/', (req, res) => {
   const { error } = validateGenre(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
   
-  const genre = {
-    id: genres.length + 1,
-    name: req.body.name
-  };
-  genres.push(genre);
+  let genre = new Genre({ name: req.body.name });
+  genre = await genre.save();
+  
   res.send(genre);
 });
   
 router.put('/:id', (req, res) => {
   const genre = genres.find(c => c.id === parseInt(req.params.id));
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-  
+
   const { error } = validateGenre(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
     
