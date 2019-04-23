9a4676417ee2e106778c41b352a580882904ee1e
n-hari159 <n.hari159@gmail.com>
2019-04-23 19:16:49 +0530

    Implementing role based authorization

--- vidly/routes/genres.js
+++ vidly/routes/genres.js
@@ -1,3 +1,4 @@
+const admin = require('../middleware/admin');
 const auth = require('../middleware/auth');
 const {Genre, validate} = require('../models/genre');
 const mongoose = require('mongoose');
@@ -32,7 +33,7 @@ router.put('/:id', async (req, res) => {
   res.send(genre);
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', [auth, admin], async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
 
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
